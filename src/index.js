16c3d4d1b374ed593cedc78a17327d3c12c8d95d
Mike Leonard <mtleonardjr@gmail.com>
2020-02-22 13:08:18 -0500

    added profile data

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import me from './me.jpg'
-
+import me from './me.jpg';
 
 var data = {
-    name: 'Michael Leonard',
-    imgURL: me,
-    hobbyList: ['Backpacking',
-                'Going to the movies',
-                'Playing videogames']
-}
+  name: 'Michael Leonard',
+  imgURL: me,
+  hobbyList: ['Backpacking', 'Going to the movies', 'Playing videogames'],
+  testvar: 'This is a test',
+  education: {
+    name: 'Towson University',
+    degree: 'B.S. of Computer Science',
+    honors: ['Cum Laude', "Dean's List /(all semesters/)"],
+    from: '01/08/2017',
+    to: '01/12/2020'
+  }
+};
 
-ReactDOM.render(<App data={data}/>, document.getElementById('root'));
-
-
-
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
