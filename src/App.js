ffae6e291620f52f9ab853eb2872f134094b943f
Mike Leonard <mtleonardjr@gmail.com>
2020-02-25 12:02:36 -0500

    finished card albums

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Card data={this.props.data} />
+        <div className="cards">
+          <Card />
+        </div>
       </div>
     );
   }
@@ -33,21 +35,37 @@ class Profile extends Component {
 */
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      posts: []
+    };
+  }
+  componentDidMount() {
+    const url = 'https://jsonplaceholder.typicode.com/photos';
+    fetch(url)
+      .then(response => response.json())
+      .then(json => this.setState({ photos: json }));
+
+    console.log(this.state);
+  }
   render() {
-    const data = this.props.data;
-    return (
-      <div className="cards">
-        <article className="card a">
-          <img src={data.imgURL} className="image" />
-          <div>TEST</div>
-        </article>
-        <article className="card b">b</article>
-        <article className="card c">c</article>
-        <article className="card d">d</article>
-        <article className="card e">e</article>
-        <article className="card f">f</article>
-      </div>
-    );
+    const { photos } = this.state;
+    return photos.map(photo => (
+      <article className="card">
+        <div>
+          <img src={photo.url} className="image" alt="card" />
+          <div>
+            <div className="card-title">{photo.title}</div>
+            <div>
+              <span className="card-album">{photo.albumId}</span>
+              <span className="card-body">{photo.title}</span>
+            </div>
+          </div>
+        </div>
+      </article>
+    ));
   }
 }
 
@@ -65,7 +83,6 @@ class Hobbys extends Component {
     );
   }
 }
-
 */
 
 export default App;
