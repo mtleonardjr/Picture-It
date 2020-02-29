cfdcca15de6a43aebff22efab725286a78174ca5
Mike Leonard <mtleonardjr@gmail.com>
2020-02-29 11:06:50 -0500

    finished css on navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Card from './components/Card';
-//import Profile from './components/Profile';
-//import Profiles from './components/Profiles';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Card from './components/Card';
+import Profile from './components/Profile';
+import Profiles from './components/Profiles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      /*<div className="App">
-        <div className="cards">
-          <Card />
+      <Router>
+        <div>
+          <ul className="navbar">
+            <li>
+              <Link to="/">Profile</Link>
+            </li>
+            <li>
+              <Link to="/profiles">Profiles</Link>
+            </li>
+            <li>
+              <Link to="/album">Album</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            {/*Album Route */}
+            <Route path="/album">
+              <div className="App">
+                <div className="cards">
+                  <Card />
+                </div>
+              </div>
+            </Route>
+            {/*ProfileRoute */}
+            <Route exact path="/">
+              <Profile />
+            </Route>
+            <Route to path="/profiles">
+              <Profiles />
+            </Route>
+          </Switch>
         </div>
-      </div> */
-      /*<Profile />
-      <Profiles /> */
+      </Router>
     );
   }
 }
